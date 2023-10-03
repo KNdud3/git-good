7be09fd3a13ce1cc2bb837d01baa6ae61c547e5b
Krishan <Krishan.Sachdev@warwick.ac.uk>
2023-10-03 18:26:55 +0100

    new changes

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,34 +1,15 @@
 
-public class Fibonacci {
-
-    public static void main(String[] args) {
-        int n = parseArgs(args);
-        if (n == -1) {
-            System.err.println("Invalid Arguments");
-            return;
-        }
-
-        FibonacciCalculator fibonacciCalculator = new FibonacciCalculator();
-
-        for (int i = 0; i < n - 1; i++) {
-            System.out.print(fibonacciCalculator.calculate(i) + ", ");
-        }
-        System.out.println(fibonacciCalculator.calculate(n - 1));
+public static void main(String[] args) {
+    int n = parseArgs(args);
+    if (n == -1) {
+        System.err.println("Invalid Arguments");
+        return;
     }
 
-    private static Integer parseArgs(String[] args) {
-        if (args.length != 1 || args[0] == null)
-            return -1;
-
-        int n;
-        try {
-            n = Integer.parseInt(args[0]);
-        } catch (NumberFormatException e) {
-            return -1;
-        }
-
-        if (n < 0)
-            return -1;
-        return n;
+    FibonacciGenerator fibonacciGenerator = new FibonacciGenerator(n);
+    while (fibonacciGenerator.hasNext()) {
+        System.out.print(fibonacciGenerator.next() + ", ");
     }
+    System.out.println();
 }
+
